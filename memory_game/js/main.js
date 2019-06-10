a587008b4e1f7058a634fcc017066f235cf4caf9
Scott <eaglendahawk98@gmail.com>
2019-06-10 11:52:29 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and Running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardThree);
